6eed95440517502e972f39b9403666193de8fdb6
Eugene Wilson <wilsonet@upmc.edu>
2015-08-07 17:09:52 -0400

    delete list

--- linked_lists/singly_linked_list.py
+++ linked_lists/singly_linked_list.py
@@ -11,17 +11,20 @@ def insertInFront(head, data):
   return newElement
 
 def deleteElement(head, data):
-  element = find(head, data)
-  if element:
-    
-      
+  element = head
+  previous = head
+  while(element):
+    if (element.data == data):
+      head = previous
+      head.next = element.next
+      break
+    previous = element
+    element = element.next
 
 def find(head, data):
   element = head
   while(element and element.data != data):
     element = element.next
-    if (element):
-        break
   return element
 
 head = ListElement("foo")
@@ -29,6 +32,9 @@ head = insertInFront(head, "bar")
 head = insertInFront(head, "baz")
 
 print find(head, "baz").data
+print find(head, "bing")
+
+deleteElement(head, "baz")
 
 
 
