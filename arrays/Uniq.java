22495d8ddbf68d52f706c419092b9a48d1d0357d
Eugene Wilson <etwilson@gmail.com>
2015-10-03 18:32:42 -0400

    Java stuff

---
+++ arrays/Uniq.java
@@ -0,0 +1,15 @@
+public class Uniq {
+
+  public boolean is_uniq(String str) {
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Uniq u = new Uniq();
+    String s = "asdfasdflkjasf";
+    System.out.println(u.is_uniq(s));
+  }
+
+
+}
+
---
+++ scratch/Foo.java
@@ -0,0 +1,11 @@
+public class Foo {
+
+  public static void main(String[] args) {
+    int[] numbers = new int[] {20, 1, 42, 15, 34};
+    for (int n : numbers) {
+      System.out.println(n);
+    }
+  }
+
+}
+
