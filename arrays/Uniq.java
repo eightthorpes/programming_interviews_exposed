299559767069507714dc15fabc500874ea074f85
Eugene Wilson <etwilson@gmail.com>
2015-10-04 23:30:21 -0400

    added uniq

--- arrays/Uniq.java
+++ arrays/Uniq.java
@@ -1,15 +1,25 @@
+import java.util.Arrays;
+
 public class Uniq {
 
   public boolean is_uniq(String str) {
+    if (str.length() < 2) {
+      return true;
+    }
+    char[] chars = str.toCharArray();
+    Arrays.sort(chars);
+    for (int i = 1; i < chars.length; i++) {
+      if (chars[i] == chars[i - 1]) {
+        return false;
+      }
+    }
     return true;
   }
 
   public static void main(String[] args) {
     Uniq u = new Uniq();
-    String s = "asdfasdflkjasf";
-    System.out.println(u.is_uniq(s));
+    System.out.println(u.is_uniq(args[0]));
   }
 
 
 }
-
