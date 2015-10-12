3a8df0e3ee42b8749d369752039cd983cb103cec
Eugene Wilson <etwilson@gmail.com>
2015-10-11 21:12:14 -0400

    deduplicate list

--- linked_lists/Node.java
+++ linked_lists/Node.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 
+
 public class Node {
   Node next = null;
   int data;
@@ -17,44 +18,56 @@ public class Node {
     n.next = end;
   }
 
-  Node deleteNode(Node head, int d) {
-    Node n = head;
+  void deleteNode(int d) {
+    Node n = this;
 
     if (n.data == d) {
-      return head.next;
+      n = n.next;
+      return;
     }
 
     while (n.next != null) {
       if (n.next.data == d) {
         n.next = n.next.next;
-        return head;
+        return;
       }
       n = n.next;
     }
-    return head;
   }
 
-  Node deDuplicate(Node head) {
-    // At each node, start from the beginning and check all previous
-    Node n = head;
-
-    while (n.next != null) {
-      Node o = head;
-      while (n != o) {
-        if (n.data == o.data) {
-          //delete node
-        }
-
+  void print() {
+    Node n = this;
+    while (n != null) {
+      System.out.println(n.data);
+      n = n.next;
     }
   }
-    
+
+  void deDuplicate() {
+    // At each node, start from here and check until the end
+    Node n = this;
+    while (n.next != null) {
+      Node o = n;
+      while (o.next != null) {
+        if (o.next.data == n.data) {
+          o.next = o.next.next;
+        }
+        o = o.next;
+      }
+      n = n.next;
+    }
+    return;
+  }
 
   public static void main(String[] args) {
     Node p = new Node(0);
-    int[] arr = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377};
+    int[] arr = {1, 1, 2, 3, 5, 8, 13, 21, 34, 8, 5, 55, 89, 144, 233, 377};
     for (int i : arr) {
       p.appendToTail(i);
     }
+    p.print();
+    p.deDuplicate();
+    p.print();
   }
 
 }
